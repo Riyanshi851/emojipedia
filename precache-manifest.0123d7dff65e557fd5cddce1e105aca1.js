a203ad9d97b7ef3d817081dd76020ade0ed2396a
Riyanshi <riyanshi.shah@gmail.com>
2020-07-23 20:13:09 +0530

    Updates

---
+++ precache-manifest.0123d7dff65e557fd5cddce1e105aca1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166d60687d1fa8ae8b9cff8e1cb693a3",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "702d53259c9c284097b4",
+    "url": "/emojipedia/static/js/2.5bb1c6f2.chunk.js"
+  },
+  {
+    "revision": "a18438114af71004fb74",
+    "url": "/emojipedia/static/js/main.97344bab.chunk.js"
+  },
+  {
+    "revision": "7ea935b83df6c53ba113",
+    "url": "/emojipedia/static/js/runtime-main.d52a7a0e.js"
+  }
+]);
